df1f0c89f2aaa6a340f2321f2a0e65a15f8763ce
EmiliannoDickel <emilianno.dickel@universo.univates.br>
2025-07-24 22:09:10 -0300

    feat: criado entidade Voto e relacionamentos com Pauta e Associado

--- src/main/java/com/desafio/votacao/model/Voto.java
+++ src/main/java/com/desafio/votacao/model/Voto.java
@@ -1,4 +1,25 @@
 package com.desafio.votacao.model;
 
+import com.desafio.votacao.enums.EscolhaVoto;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Voto")
 public class Voto {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String cpfAssociado;
+    @Enumerated(EnumType.STRING)
+    private EscolhaVoto voto;
+    @ManyToOne
+    @JoinColumn(name = "pauta_id")
+    private Pauta pauta;
+
 }
