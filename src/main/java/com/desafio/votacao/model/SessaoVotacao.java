bcb1fc3d57cd4f3012a3a49ab9eee1cb8c1c8d0f
EmiliannoDickel <emilianno.dickel@universo.univates.br>
2025-07-24 22:11:50 -0300

    feat: criado entidade SessaoVotacao

--- src/main/java/com/desafio/votacao/model/SessaoVotacao.java
+++ src/main/java/com/desafio/votacao/model/SessaoVotacao.java
@@ -1,4 +1,24 @@
 package com.desafio.votacao.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table (name = "Sessao")
 public class SessaoVotacao {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private LocalDateTime dataAbertura;
+    private LocalDateTime dataFechamento;
+
+    @OneToOne
+    @JoinColumn(name = "pauta_id")
+    private Pauta pauta;
 }
