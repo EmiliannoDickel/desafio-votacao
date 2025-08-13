18d46d39deddee69bc75502c86d9722c2da9ae4e
EmiliannoDickel <emilianno.dickel@universo.univates.br>
2025-08-12 21:58:41 -0300

    feat: Adicionado @ControllerAdvice para centralizar os tratamentos de erros

--- src/main/java/com/desafio/votacao/exception/ManipuladorGlobalDeExcecoes.java
+++ src/main/java/com/desafio/votacao/exception/ManipuladorGlobalDeExcecoes.java
@@ -1,4 +1,20 @@
 package com.desafio.votacao.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class ManipuladorGlobalDeExcecoes {
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<String> handleIllegalStateException(IllegalStateException e){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
 }
