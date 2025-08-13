f22d370f138bc0fa56c79f2f46cb41112c8c1c64
EmiliannoDickel <emilianno.dickel@universo.univates.br>
2025-08-12 21:56:08 -0300

    feat: adicionado ResponseEntity para controle das respostas HTTP

--- src/main/java/com/desafio/votacao/controller/SessaoVotacaoController.java
+++ src/main/java/com/desafio/votacao/controller/SessaoVotacaoController.java
@@ -1,4 +1,24 @@
 package com.desafio.votacao.controller;
 
+import com.desafio.votacao.model.SessaoVotacao;
+import com.desafio.votacao.service.SessaoVotacaoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/sessoes")
 public class SessaoVotacaoController {
+
+    private final SessaoVotacaoService sessaoVotacaoService;
+
+    public SessaoVotacaoController(SessaoVotacaoService sessaoVotacaoService) {
+        this.sessaoVotacaoService = sessaoVotacaoService;
+    }
+
+    @PostMapping("/abrir/{id}")
+    public ResponseEntity<SessaoVotacao> abrirSessao (@PathVariable Long id, @RequestParam (required = false) Integer duracaoPauta) {
+        SessaoVotacao sessao = sessaoVotacaoService.abrirSessao(id, duracaoPauta);
+        return ResponseEntity.ok(sessao);
+    }
 }
