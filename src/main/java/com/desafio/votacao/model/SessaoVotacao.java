87d18a5e90d112e7174dfd3fbe9e0e808709d71a
EmiliannoDickel <emilianno.dickel@universo.univates.br>
2025-08-12 22:01:06 -0300

    fix: adicionado @jsonignore no atributo pauta, pois estava gerando um looping na requisição

--- src/main/java/com/desafio/votacao/model/SessaoVotacao.java
+++ src/main/java/com/desafio/votacao/model/SessaoVotacao.java
@@ -1,7 +1,9 @@
 package com.desafio.votacao.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,14 +13,22 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table (name = "Sessao")
+@Builder
+@Table (name = "sessao_votacao")
 public class SessaoVotacao {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private LocalDateTime dataAbertura;
     private LocalDateTime dataFechamento;
 
     @OneToOne
-    @JoinColumn(name = "pauta_id")
+    @JsonIgnore
+    @JoinColumn(name = "pauta_id", nullable = false, unique = true)
     private Pauta pauta;
+
+    public boolean estaAberta(){
+        LocalDateTime time = LocalDateTime.now();
+        return time.isAfter(dataAbertura) && time.isBefore(dataFechamento);
+    }
 }
