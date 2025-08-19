c230370c3340185bfdd58b1e523cf431f1665efd
EmiliannoDickel <emilianno.dickel@universo.univates.br>
2025-08-19 19:49:18 -0300

    feat: adicionado dto ResultadoVotacao, para contagem dos votos e resposta dos mesmos

--- src/main/java/com/desafio/votacao/dto/ResultadoVotacao.java
+++ src/main/java/com/desafio/votacao/dto/ResultadoVotacao.java
@@ -1,4 +1,34 @@
 package com.desafio.votacao.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ResultadoVotacao {
+    private Long pautaId;
+    private String tituloPauta;
+    private Long totalVotos;
+    private Long totalSim;
+    private Long totalNao;
+    private String resultadoFinal;
+
+    public ResultadoVotacao(Long pautaId, String tituloPauta, long totalSim, long totalNao) {
+        this.pautaId = pautaId;
+        this.tituloPauta = tituloPauta;
+        this.totalSim = totalSim;
+        this.totalNao = totalNao;
+        this.totalVotos = totalSim + totalNao;
+        this.resultadoFinal = calcularResultadoFinal(totalSim, totalNao);
+    }
+
+    public String calcularResultadoFinal(long totalSim, long totalNao) {
+        if (totalSim > totalNao) {
+            return "Aprovada!";
+        } else if (totalSim < totalNao) {
+            return "Rejeitada!";
+        } else {
+            return "Empate!";
+        }
+    }
 }
