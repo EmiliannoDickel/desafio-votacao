1182a06322c82fe12ea301a8fe6a9252d8be282c
EmiliannoDickel <emilianno.dickel@universo.univates.br>
2025-08-19 19:47:35 -0300

    feat: criado exception VotoRegistradoException, para padronizar uma resposta a API de associados querendo fazer votação duplicada

--- src/main/java/com/desafio/votacao/exception/VotoRegistradoException.java
+++ src/main/java/com/desafio/votacao/exception/VotoRegistradoException.java
@@ -1,4 +1,7 @@
 package com.desafio.votacao.exception;
 
-public class VotoRegistradoException {
+public class VotoRegistradoException extends IllegalStateException{
+    public VotoRegistradoException(String mensagem){
+        super(mensagem);
+    }
 }
