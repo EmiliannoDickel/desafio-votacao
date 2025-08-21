7378024f31f23e13b3693f95f95e1e22f5b024d8
EmiliannoDickel <emilianno.dickel@universo.univates.br>
2025-08-20 21:12:06 -0300

    refactor: alterei no PautaService as chamadas para exceptions, com exceptions próprias para cada erro, coloquei CampoObrigatorioException

--- src/main/java/com/desafio/votacao/service/PautaService.java
+++ src/main/java/com/desafio/votacao/service/PautaService.java
@@ -1,5 +1,5 @@
 package com.desafio.votacao.service;
-import com.desafio.votacao.exception.TituloDescricaoNullException;
+import com.desafio.votacao.exception.CampoObrigatorioException;
 import com.desafio.votacao.model.Pauta;
 import com.desafio.votacao.repository.PautaRepository;
 import org.springframework.stereotype.Service;
@@ -7,11 +7,18 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PautaService {
-    private PautaRepository pautaRepository;
+    private final PautaRepository pautaRepository;
+
+    public PautaService(PautaRepository pautaRepository) {
+        this.pautaRepository = pautaRepository;
+    }
 
     public Pauta salvar (Pauta pauta) {
-        if (pauta.getTitulo() == null || pauta.getDescricao() == null) {
-            throw new TituloDescricaoNullException("Título ou Descrição não informado!");
+        if (pauta.getTitulo() == null || pauta.getTitulo().isEmpty()) {
+            throw new CampoObrigatorioException("Título não informado ou em branco!");
+        }
+        if (pauta.getDescricao() == null || pauta.getDescricao().isEmpty()) {
+            throw new CampoObrigatorioException("Descricão não informada ou em branco!");
         }
         return pautaRepository.save(pauta);
     }
