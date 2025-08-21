9e34c73327fb95e3e09c324d83381faa502f9ac5
EmiliannoDickel <emilianno.dickel@universo.univates.br>
2025-08-20 21:10:41 -0300

    chore: adicionei a tag @Repository no SessaoVotacaoRepository

--- src/main/java/com/desafio/votacao/repository/SessaoVotacaoRepository.java
+++ src/main/java/com/desafio/votacao/repository/SessaoVotacaoRepository.java
@@ -2,6 +2,8 @@ package com.desafio.votacao.repository;
 
 import com.desafio.votacao.model.SessaoVotacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SessaoVotacaoRepository extends JpaRepository<SessaoVotacao, Long> {
 }
