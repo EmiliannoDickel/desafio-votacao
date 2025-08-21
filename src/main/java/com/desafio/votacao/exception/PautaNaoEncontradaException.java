688776b54f5e5efe1d5b44cc0e46ade428913a7b
EmiliannoDickel <emilianno.dickel@universo.univates.br>
2025-08-20 21:08:56 -0300

    feat: criado PautaNaoEncontradaException

--- src/main/java/com/desafio/votacao/exception/PautaNaoEncontradaException.java
+++ src/main/java/com/desafio/votacao/exception/PautaNaoEncontradaException.java
@@ -1,4 +1,7 @@
 package com.desafio.votacao.exception;
 
-public class PautaNaoEncontradaException {
+public class PautaNaoEncontradaException extends RuntimeException{
+    public PautaNaoEncontradaException(String mensagem){
+        super(mensagem);
+    }
 }
