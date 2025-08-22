3d2c0c1b2dd6c1c3241aae7ab114d17485b5049d
EmiliannoDickel <emilianno.dickel@universo.univates.br>
2025-08-21 22:37:07 -0300

    feat: começado a adicionar as tag para organização do swagger

---
+++ src/main/java/com/desafio/votacao/config/SwaggerConfiguracao.java
@@ -0,0 +1,4 @@
+package com.desafio.votacao.config;
+
+public class SwaggerConfiguracao {
+}
--- src/main/java/com/desafio/votacao/controller/PautaController.java
+++ src/main/java/com/desafio/votacao/controller/PautaController.java
@@ -2,12 +2,17 @@ package com.desafio.votacao.controller;
 
 import com.desafio.votacao.model.Pauta;
 import com.desafio.votacao.service.PautaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/pautas")
+@Tag(name = "Pauta", description = "Responsável pelo gerencimaneto da das pautas.")
 public class PautaController {
 
     private final PautaService pautaService;
@@ -18,6 +23,8 @@ public class PautaController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Criar Pauta", description = "rota responsável pela criação da Pauta, com nome e descrição.")
+    @ApiResponse(responseCode = "201", description = "Pauta criada com sucesso")
     public ResponseEntity<Pauta> criar (@RequestBody Pauta pauta) {
         Pauta novaPauta = pautaService.salvar(pauta);
         return ResponseEntity.status(HttpStatus.CREATED).body(novaPauta);
