fc0f3b8d712aca62fd64624a49292ba6d9611c36
EmiliannoDickel <emilianno.dickel@universo.univates.br>
2025-08-21 22:37:51 -0300

    feat: criado SwaggerConfiguracao para organização do swagger

--- src/main/java/com/desafio/votacao/config/SwaggerConfiguracao.java
+++ src/main/java/com/desafio/votacao/config/SwaggerConfiguracao.java
@@ -1,4 +1,28 @@
 package com.desafio.votacao.config;
 
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfiguracao {
+
+    @Bean
+    public OpenAPI getOpenAPI() {
+        Contact contato = new Contact();
+        contato.name("Emilianno Dickel");
+        contato.email("emidudickel@gmail.com");
+        contato.url("https://github.com/EmiliannoDickel/desafio-votacao");
+
+        Info info = new Info();
+        info.title("desafio votação");
+        info.version("1.0");
+        info.description("Desafio para votação de associados em uma Pauta");
+        info.contact(contato);
+
+        return new OpenAPI().info(info);
+    }
 }
+
