b6f1ad2212041db2a1c767b1d1fb3f24e1db9471
Emilianno <emidudickel@gmail.com>
2025-08-24 22:57:39 -0300

    feat: criado DTO PautaResultadoDTO, para resposta do corpo de criado pelo usuario.

--- src/main/java/com/desafio/votacao/dto/PautaResultadoDTO.java
+++ src/main/java/com/desafio/votacao/dto/PautaResultadoDTO.java
@@ -1,4 +1,21 @@
 package com.desafio.votacao.dto;
 
+import com.desafio.votacao.model.Pauta;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PautaResultadoDTO {
+    private Long id;
+    private String titulo;
+    private String descricao;
+
+    public static PautaResultadoDTO dePauta(Pauta pauta) {
+        return new PautaResultadoDTO(pauta.getId(), pauta.getTitulo(), pauta.getDescricao());
+    }
 }
