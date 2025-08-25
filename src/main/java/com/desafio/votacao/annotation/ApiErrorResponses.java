f17a0e3a83e7d5ffc005aed71450420db06c1b66
Emilianno <emidudickel@gmail.com>
2025-08-24 22:28:31 -0300

    feat: criado Annotation ApiErrorResponse para documentação do swagger

--- src/main/java/com/desafio/votacao/annotation/ApiErrorResponses.java
+++ src/main/java/com/desafio/votacao/annotation/ApiErrorResponses.java
@@ -1,4 +1,38 @@
 package com.desafio.votacao.annotation;
 
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.ProblemDetail;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@ApiResponses(
+        value = {
+                @ApiResponse(
+                        responseCode = "400",
+                        description = "Erro de validação",
+                        content = @Content(schema = @Schema(implementation = ProblemDetail.class))
+                ),
+                @ApiResponse(
+                        responseCode = "404",
+                        description = "Recurso não encontrado",
+                        content = @Content(schema = @Schema(implementation = ProblemDetail.class))
+                ),
+                @ApiResponse(
+                        responseCode = "500",
+                        description = "Erro interno do servidor",
+                        content = @Content(schema = @Schema(implementation = ProblemDetail.class))
+                )
+        }
+)
 public @interface ApiErrorResponses {
 }
