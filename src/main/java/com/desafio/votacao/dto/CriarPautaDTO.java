bd7c43b8c851b74f4a4fd3a327dc4d3d54deb866
Emilianno <emidudickel@gmail.com>
2025-08-24 22:54:47 -0300

    feat: adicionado DTO CriarPautaDTO, para controle e retorno correto da requisição a API

--- src/main/java/com/desafio/votacao/dto/CriarPautaDTO.java
+++ src/main/java/com/desafio/votacao/dto/CriarPautaDTO.java
@@ -1,4 +1,29 @@
 package com.desafio.votacao.dto;
 
+import com.desafio.votacao.model.Pauta;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CriarPautaDTO {
+    @NotNull(message = "Campo título não pode ser Null")
+    @NotBlank(message = "Campo título não pode ser Vazio")
+    private String titulo;
+    @NotNull(message = "Campo descrição não pode ser Null")
+    @NotBlank(message = "Campo descrição não pode ser Vazio")
+    private String descricao;
+
+    public Pauta paraPauta() {
+        Pauta pauta = new Pauta();
+        pauta.setTitulo(this.titulo);
+        pauta.setDescricao(this.descricao);
+        return pauta;
+    }
 }
