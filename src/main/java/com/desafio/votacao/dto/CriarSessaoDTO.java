0cc3c0bf227cb0a366d49e8fe954af253e33455a
EmiliannoDickel <emilianno.dickel@universo.univates.br>
2025-08-28 20:02:22 -0300

    feat: criado CriarSessaoDTO e SessaoResultadoDTO

--- src/main/java/com/desafio/votacao/dto/CriarSessaoDTO.java
+++ src/main/java/com/desafio/votacao/dto/CriarSessaoDTO.java
@@ -1,4 +1,32 @@
 package com.desafio.votacao.dto;
 
+import com.desafio.votacao.model.SessaoVotacao;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CriarSessaoDTO {
+
+    @NotNull(message = "Data de abertura não pode ser nula")
+    @Future(message = "Data de abertura deve ser uma data futura")
+    private LocalDateTime dataAbertura;
+    @NotNull(message = "Data de fechamento não pode ser nula")
+    @Future(message = "Data de fechamento deve ser uma data futura")
+    private LocalDateTime dataFechamento;
+
+    public SessaoVotacao paraSessaoVotacao () {
+        SessaoVotacao sessaoVotacao = new SessaoVotacao();
+        sessaoVotacao.setDataAbertura(dataAbertura);
+        sessaoVotacao.setDataFechamento(dataFechamento);
+        return sessaoVotacao;
+    }
 }
--- src/main/java/com/desafio/votacao/dto/SessaoResultadoDTO.java
+++ src/main/java/com/desafio/votacao/dto/SessaoResultadoDTO.java
@@ -1,4 +1,26 @@
 package com.desafio.votacao.dto;
 
+import com.desafio.votacao.model.SessaoVotacao;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SessaoResultadoDTO {
+    private long id;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime dataAbertura;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime dataFechamento;
+
+    public static SessaoResultadoDTO deSessao (SessaoVotacao sessao) {
+        return new SessaoResultadoDTO(sessao.getId(), sessao.getDataAbertura(), sessao.getDataFechamento());
+    }
 }
